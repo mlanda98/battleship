9c8350c9606733c0da5196dac12608ccb2d93c77
Mayra Landa <mlandae16@gmail.com>
2024-06-12 17:15:29 -0700

    add function for sunk and test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-plusplus */
 class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
-    this.sunk = false;
+  }
+
+  hit() {
+    this.hits++;
   }
 }
+
+function isSunk(ship) {
+  return ship.hits >= ship.length;
+}
+
+module.exports = {
+  Ship,
+  isSunk,
+};
